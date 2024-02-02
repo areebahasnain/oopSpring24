f41bc225f593fd663234109afe1941fd2fc48d21
areebahasnain <142868074+areebahasnain@users.noreply.github.com>
2024-02-02 23:49:01 +0500

    Add files via upload

---
+++ Lab 02/Q1.cpp
@@ -0,0 +1,29 @@
+/* Task Description: swap two numbers using recursive function */
+
+#include <iostream>
+
+using namespace std;
+
+void recursiveSwap(int &a, int &b, int count) {  
+    if (count == 0) {  
+        return;  
+    } else {  
+        int temp = a;  
+        a = b;  
+        b = temp;  
+        recursiveSwap(a, b, count-1);  
+    }  
+}  
+
+int main() {
+    int x, y;
+    cout << "Enter a number: ";
+    cin >> x;
+    cout << "Enter a number: ";
+    cin >> y;    
+    cout << "Before: x = " << x << " y = " << y << endl;  
+
+    recursiveSwap(x, y, 1);
+    cout << "After: x = " << x << " y = " << y << endl;  
+    return 0;  
+} 
